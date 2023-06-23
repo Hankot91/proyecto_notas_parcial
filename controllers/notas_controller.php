6bc74abae20177a7baa9d2aac095c86053377d47
Hankot91 <gildardo991>
2023-06-23 02:54:28 -0500

    implementacion del modelo y controlador de notas

---
+++ controllers/notas_controller.php
@@ -0,0 +1,89 @@
+<?php
+
+require_once __DIR__ . '/../connection/connection.php';
+require_once __DIR__ . '/../models/notas.php';
+require_once "../controllers/controller.php";
+
+
+class NotasController implements Controller
+{
+    private $dbConnection;
+    private $notasModel;
+
+    public function __construct()
+    {
+        $this->dbConnection = DatabaseConnection::getInstance();
+        $this->notasModel = new Notas($this->dbConnection);
+    }
+
+    public function handleRequest()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['agregar'])) {
+                $this->handleCreate();
+            } elseif (isset($_POST['actualizar'])) {
+                $this->handleUpdate();
+            } elseif (isset($_POST['eliminar'])) {
+                $this->handleDelete();
+            }
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            if (isset($_GET['buscar_submit'])) {
+                return $this->notasModel->getNota($_GET['buscar']);
+            }
+        }
+            return $this->handleReturnAll();
+    }
+
+    public function handleReturnAll()
+    {
+        return $this->notasModel->getAllNotas();
+    }
+
+    public function handleReturnCursos(){
+        return $this->notasModel->getCursos();
+    }
+
+    public function handleCreate()
+    {
+        $nota = $_POST['nota'];
+        $descripcionNota = $_POST['descrip_nota'];
+        $porcentaje = $_POST['porcentaje'];
+        $posicion = $_POST['posicion'];
+        $codCurso = $_POST['cod_cur'];
+        $notaExistente = $this->notasModel->getNota($nota);
+        if ($notaExistente) {
+            // El código de nota ya existe, mostrar mensaje de error
+            echo "<script>
+                                document.addEventListener('DOMContentLoaded', function() {
+                                            let parrafo = document.querySelector('.mensaje_error');
+                                            if (parrafo) {
+                                            parrafo.textContent = 'El código de nota ya existe. Por favor, elige otro código.';
+                                        }
+                                });
+                            </script>";
+        } else {
+            $this->notasModel->createNota($nota, $descripcionNota, $porcentaje, $posicion, $codCurso);
+        }
+    }
+
+    public function handleUpdate()
+    {
+        $nota = $_POST['nota'];
+        $descripcionNota = $_POST['descrip_nota'];
+        $porcentaje = $_POST['porcentaje'];
+        $posicion = $_POST['posicion'];
+        $codCurso = $_POST['cod_cur'];
+        $this->notasModel->updateNota($nota, $descripcionNota, $porcentaje, $posicion, $codCurso);
+    }
+
+    public function handleDelete()
+    {
+        $nota = $_POST['nota'];
+        $this->notasModel->deleteNota($nota);
+    }
+
+
+
+}
---
+++ models/notas.php
@@ -0,0 +1,68 @@
+<?php
+//Trayendo el paquete de conecion con la db
+require_once __DIR__ . '/../connection/connection.php';
+require_once "cursos.php";
+
+class Notas{
+
+    private $dbConnection;
+    private $cursosModel; 
+    public function __construct(DatabaseConnection $dbConnection)
+    {
+        $this->dbConnection = $dbConnection;
+        $this->cursosModel = new Cursos($dbConnection);
+    }
+
+    public function createNota($nota, $descripcionNota, $porcentaje, $posicion, $codCurso)
+    {
+        $query = "INSERT INTO notas (nota, descrip_nota, porcentaje, posicion, cod_cur) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $this->dbConnection->getConnection()->prepare($query);
+        $stmt->execute([$nota, $descripcionNota, $porcentaje, $posicion, $codCurso]);
+    }
+
+    public function getAllNotas()
+    {
+        $query = "SELECT * FROM notas";
+        $stmt = $this->dbConnection->getConnection()->query($query);
+        $notasData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $notasData;
+    }
+
+    public function getNota($busqueda)
+    {
+        $query = "SELECT * FROM notas
+        WHERE nota = ? OR
+                descrip_nota = ? OR
+                porcentaje = ? OR
+                posicion = ? OR
+                cod_cur = ?";
+
+        $stmt = $this->dbConnection->getConnection()->prepare($query);
+        $stmt->execute([$busqueda, $busqueda, $busqueda, $busqueda, $busqueda]);
+        $notasData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        var_dump($notasData);
+        return $notasData;
+    }
+
+    public function updateNota($nota, $descripcionNota, $porcentaje, $posicion, $codCurso)
+    {
+        $query = "UPDATE notas SET descrip_nota = ?, porcentaje = ?, posicion = ?, cod_cur = ? WHERE nota = ?";
+        $stmt = $this->dbConnection->getConnection()->prepare($query);
+        $stmt->execute([$descripcionNota, $porcentaje, $posicion, $codCurso, $nota]);
+    }
+
+    public function deleteNota($nota)
+    {
+        $query = "DELETE FROM notas WHERE nota = ?";
+        $stmt = $this->dbConnection->getConnection()->prepare($query);
+        $stmt->execute([$nota]);
+    }
+
+    public function getCursos(){
+        return $this->cursosModel->getAllCursos();
+    }
+    
+}
+
+
+?>
